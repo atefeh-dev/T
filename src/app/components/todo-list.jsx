ff86aed3d6a04c9c40be87ce5f93cd6df41f2de2
Atefeh <atefeh@Atefehs-MacBook-Air.local>
2023-12-14 12:27:25 +0330

    feat(TodoList): implement TodoList component

---
+++ src/app/components/todo-list.jsx
@@ -0,0 +1,23 @@
+// src/components/TodoList.js
+import { useSelector } from "react-redux";
+import TodoItem from "./TodoItem";
+
+const TodoList = () => {
+  const todos = useSelector((state) => {
+    return state.todos.filter((todo) => {
+      if (state.filter === "all") return true;
+      if (state.filter === "completed") return todo.completed;
+      return !todo.completed;
+    });
+  });
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
