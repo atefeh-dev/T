4532cc10c85ba7e383b5f2058071e40bd666f3d9
Atefeh <atefeh@Atefehs-MacBook-Air.local>
2023-12-14 12:52:36 +0330

    feat(TodoForm): implement TodoForm component

---
+++ src/app/components/todo-form.jsx
@@ -0,0 +1,30 @@
+// src/components/TodoForm.js
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodoAsync } from "../redux/todos";
+
+const TodoForm = () => {
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim() !== "") {
+      dispatch(addTodoAsync(text));
+      setText("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
+
+export default TodoForm;
